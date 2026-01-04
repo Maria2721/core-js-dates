5b0ecb7ea8e4c376464b517aba095730d08b269d
Maria2721 <mashaivvas@gmail.com>
2026-01-05 02:53:42 +0300

    feat: add getNextFriday task

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -80,8 +80,15 @@ function getDayName(date) {
  * Date('2024-02-13T00:00:00Z') => Date('2024-02-16T00:00:00Z')
  * Date('2024-02-16T00:00:00Z') => Date('2024-02-23T00:00:00Z')
  */
-function getNextFriday(/* date */) {
-  throw new Error('Not implemented');
+function getNextFriday(date) {
+  const result = new Date(date);
+  const day = result.getUTCDay();
+
+  const daysNextFriday = day === 5 ? 7 : (5 - day + 7) % 7;
+  result.setUTCDate(result.getUTCDate() + daysNextFriday);
+  result.setUTCHours(0, 0, 0, 0);
+
+  return result;
 }
 
 /**
